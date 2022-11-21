70d8dc7101b2d36b8ccdae42e80f092092bdbf4e
Projektlife <mathiskaehler@gmx.de>
2022-11-21 19:44:29 +0100

    Nochmal weitestgehend von vorne angefangen. Scheint näher an der Lösung zu sein als die Version davor.

--- webserver.c
+++ webserver.c
@@ -5,68 +5,69 @@
 #include <stdio.h>
 #include <netinet/in.h>
 
+// erstellen der Serverstruktur
+struct Server{
+    int port;
+};
 
-typedef struct res res;
 
 int main(int argc, char** argv) {
+
     // Start here :)
+    int port = port; //port wird von Client übergeben(Aufgabenstellung)
 
     // Create socket:af: address Family, type: specification for the new socket, protocol
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    printf("Reply\r\n");
 
 
     // Construct local address structure
     struct sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8001);
+    serverAddress.sin_port = htons(8000);//port wird von Client übergeben(Aufgabenstellung)
     serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    //get address info
-    struct addrinfo hints, *res;
-    getaddrinfo(NULL, "3490", 0, &res);
 
-    // Bind the socket
-    //int size= sizeof(serverAddress);
-
-    //bind(serverSocket, (struct sockaddr*) &server_address, sizeof(server_address));
-    int binding = bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
-    listen(serverSocket, 5);
-
-    // Listen on the socket
-    int listening = listen(serverSocket, backlog);
-    if (listening < 0){
-        printf("Der server ist nicht erreichbar!\n");
-        return 1;
-        }
-
-    // Accept a connection & wait for a connection
-    while(1) {
-        clientSocket = accept(serverSocket, NULL, NULL);
-        send(clientSocket, httpHeader, sizeof(httpHeader), 0);
-        close(clientSocket);
-    }
-    return 0;
-
-    // connect
-    int connect(int socket, const struct sockaddr *address, socklen_t address_len){
-
-    }
-    //address info
-    int getaddrinfo( const char *hostname, const char *servname, const char *servname, const struct addrinfo *hints, struct addrinfo **res){
-
-    }
-    int send(){
-
-    }
-    int recv(){
-
+    //teste ob server verbunden ist --> sonst verlasse mit 1 (standart Fehlerrückgabe)
+    if(serverSocket == 0){
+        perror("Failes to connect socket...\n");
+        exit(1);
     }
 
-    int close(){
-
+    // binde mit server --> teste ob Rückgabe von bind kleiner 0 (-1) dann error
+    if(bind(serverSocket, (struct sockaddr*) &serverAddress,sizeof(serverAddress)) < 0){
+        perror("Failed to bind \n");
+        exit(1); // exit mit 1
     }
+
+    //höre client zu
+    // zweiter int gibt an wie viele sich anstellen, hier erstmal von mir auf 10
+    //festgelegt bis weiteres bekannt
+    // + test (listen gibt bei fail auch -1 zurück)
+    if((listen(serverSocket,10)) < 0){
+        perror("Failed to listen...\n");
+        exit(1);
+    }
+    // den server akzeptieren
+    char buffer[3000];
+    int address_length= sizeof(serverAddress);
+    accept(serverSocket,(struct sockaddr* ) &serverAddress,(socklen_t *)&address_length);
+    printf("%s\n",buffer);
+
 
     return EXIT_SUCCESS;
-}
+};
+
+
+    // Listen on the socket
+    //int listening = listen(serverSocket, backlog);
+    //if (listening < 0){
+      //  printf("Der server ist nicht erreichbar!\n");
+       // return 1;
+        //}
+
+    // Accept a connection & wait for a connection
+   // while(1) {
+     //   clientSocket = accept(serverSocket, NULL, NULL);
+      //  send(clientSocket, httpHeader, sizeof(httpHeader), 0);
+        //close(clientSocket);
+
